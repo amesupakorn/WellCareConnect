7439a65200ab141943a4adf48e83426f77895e4a
65070188Mananchaya <65070188@kmitl.ac.th>
2024-10-11 00:46:52 +0700

    update service

--- wellcare/connects/urls.py
+++ wellcare/connects/urls.py
@@ -5,5 +5,22 @@ urlpatterns = [
     path('', views.HomePage.as_view(), name="home"),
     path('check/', views.CheckPage.as_view(), name="check"),
     path('check/showcheck/', views.ShowCheckPage.as_view(), name="showcheck"),
+    path('service-first/', views.ServiceFirst.as_view(), name="service-first"),
+    path('service-1/', views.ServiceFirst1.as_view(), name="service-1"),
+    path('service-2/', views.ServiceFirst2.as_view(), name="service-2"),
+    path('service-3/', views.ServiceFirst3.as_view(), name="service-3"),
+    path('service-4/', views.ServiceFirst4.as_view(), name="service-4"),
+
+    path('service-sec/', views.ServiceSecond.as_view(), name="service-sec"),
+    path('second-1/', views.ServiceSecond1.as_view(), name="second-1"),
+    path('second-2/', views.ServiceSecond2.as_view(), name="second-2"),
+    path('second-3/', views.ServiceSecond3.as_view(), name="second-3"),
+    path('second-4/', views.ServiceSecond4.as_view(), name="second-4"),
+
+    path('service-third/', views.ServiceThird.as_view(), name="service-third"),
+    path('third-1/', views.ServiceThird1.as_view(), name="third-1"),
+    path('third-2/', views.ServiceThird2.as_view(), name="third-2"),
+    path('third-3/', views.ServiceThird3.as_view(), name="third-3"),
+    path('third-4/', views.ServiceThird4.as_view(), name="third-4"),
     
 ]
--- wellcare/connects/views.py
+++ wellcare/connects/views.py
@@ -19,3 +19,93 @@ class ShowCheckPage(View):
         return render(request, 'showcheck.html',{
             
         })
+    
+class ServiceFirst(View):
+    def get(self, request):
+        return render(request, 'services/first.html',{
+            
+        })
+    
+class ServiceFirst1(View):
+    def get(self, request):
+        return render(request, 'services/first-1.html',{
+            
+        })
+    
+class ServiceFirst2(View):
+    def get(self, request):
+        return render(request, 'services/first-2.html',{
+            
+        })
+    
+class ServiceFirst3(View):
+    def get(self, request):
+        return render(request, 'services/first-3.html',{
+            
+        })
+    
+class ServiceFirst4(View):
+    def get(self, request):
+        return render(request, 'services/first-4.html',{
+            
+        })
+    
+class ServiceSecond(View):
+    def get(self, request):
+        return render(request, 'services/second.html',{
+            
+        })
+
+class ServiceSecond1(View):
+    def get(self, request):
+        return render(request, 'services/second-1.html',{
+            
+        })
+
+class ServiceSecond2(View):
+    def get(self, request):
+        return render(request, 'services/second-2.html',{
+            
+        })
+    
+class ServiceSecond3(View):
+    def get(self, request):
+        return render(request, 'services/second-3.html',{
+            
+        })
+    
+class ServiceSecond4(View):
+    def get(self, request):
+        return render(request, 'services/second-4.html',{
+            
+        })
+    
+class ServiceThird(View):
+    def get(self, request):
+        return render(request, 'services/third.html',{
+            
+        })
+
+class ServiceThird1(View):
+    def get(self, request):
+        return render(request, 'services/third-1.html',{
+            
+        })
+
+class ServiceThird2(View):
+    def get(self, request):
+        return render(request, 'services/third-2.html',{
+            
+        })
+    
+class ServiceThird3(View):
+    def get(self, request):
+        return render(request, 'services/third-3.html',{
+            
+        })
+    
+class ServiceThird4(View):
+    def get(self, request):
+        return render(request, 'services/third-4.html',{
+            
+        })
